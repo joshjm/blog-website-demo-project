91b90f2e630c9b7c8b543cd45d911210afa2ce23
Joshua <mcdojj12@gmail.com>
2020-05-08 15:48:19 +1000

    challenge 10

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -12,27 +12,42 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {content: homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home", {
+    content: homeStartingContent
+  });
 })
 
-app.get("/about", function(req, res){
-  res.render("about", {content: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", {
+    content: aboutContent
+  });
 })
 
-app.get("/contact", function(req, res){
-  res.render("contact", {content: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    content: contactContent
+  });
 })
 
-app.get("/compose", function(req, res){
-  res.render("compose", {content: ""});
+app.get("/compose", function (req, res) {
+  res.render("compose", {
+    content: ""
+  });
 })
 
-app.post("/compose", function(req, res){
-  console.log(req.body.data);
+app.post("/compose", function (req, res) {
+  console.log(req.body.titleText);
+  console.log(req.body.bodyText);
+  const postData = {
+    title: req.body.titleText,
+    body: req.body.bodyText
+  };
 })
 
 
@@ -41,6 +56,6 @@ app.post("/compose", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
