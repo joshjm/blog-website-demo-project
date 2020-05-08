051cef52999863385b73f512cdd3a86d0dbca873
Joshua <mcdojj12@gmail.com>
2020-05-08 21:57:09 +1000

    chall 18

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash"); // better to declare as: const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -45,14 +46,16 @@ app.get("/compose", function (req, res) {
 })
 
 // Dynamically create pages from requests
-app.get("/posts/:topic", function(req, res){
-  console.log(req.params.topic);
+app.get("/posts/:topic", function (req, res) {
   res.send(req.params.topic);
- posts.forEach(element => {
-   if (element.title == req.params.topic){
-     console.log("match found!");
-   }
- });
+  let topic = req.params.topic;
+  let kebabTopic = lodash.kebabCase(req.params.topic);
+  
+  posts.forEach(element => {
+    if (lodash.kebabCase(element.title) == req.params.topic) {
+      console.log("match found!");
+    }
+  });
 })
 
 app.post("/compose", function (req, res) {
